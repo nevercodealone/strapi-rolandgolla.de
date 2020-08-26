88e53a152b2a6aecd322a0ec2281427e41d20d82
Roland Golla <rolandgolla@gmail.com>
2020-08-26 18:14:42 +0200

    Markup for description ready

--- frontend/src/components/hero.vue
+++ frontend/src/components/hero.vue
@@ -6,9 +6,13 @@
       <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
         <p v-if="hero.hero.subline" v-html="hero.hero.subline" class="uppercase tracking-loose w-full"/>
         <h1 v-html="hero.hero.headline" class="my-4 text-5xl font-bold leading-tight"/>
-        <p v-html="hero.hero.description" class="leading-normal text-2xl mb-8">
-          
+        <p class="leading-normal text-2xl mb-8">
+          <VueShowdown
+          :markdown="hero.hero.description"
+          flavor="github"
+          :options="{ emoji: true }"/>  
         </p>
+        
       
         <a :href="hero.hero.button.target" target="_blank" class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">
           {{ hero.hero.button.text }}
