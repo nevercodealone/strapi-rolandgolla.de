a558bfe2e8227c79bbc8adbdcef734382e109ae3
Roland Golla <rolandgolla@gmail.com>
2020-10-12 19:17:32 +0200

    Purge tailwind.css

--- gridsome/gridsome.config.js
+++ gridsome/gridsome.config.js
@@ -1,8 +1,11 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
 
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+const postcssPlugins = [
+  tailwind(),
+]
+
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
 
 module.exports = {
   siteName: 'Roland Golla',
@@ -10,5 +13,12 @@ module.exports = {
     {
       use: "gridsome-plugin-tailwindcss"
     }
-  ]
+  ],
+  css: {
+      loaderOptions: {
+          postcss: {
+              plugins: postcssPlugins,
+          },
+      },
+  },
 }
