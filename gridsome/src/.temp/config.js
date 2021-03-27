9a68eadc4a3fc0da2d892545d9d59c337a4a1b52
Roland Golla <rolandgolla@gmail.com>
2021-03-28 00:51:25 +0100

    yarn install and fix .temp

--- gridsome/src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Roland Golla",
-  "siteUrl": "",
-  "version": "0.7.21",
-  "catchLinks": true
-}
--- gridsome/src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1606002649045
