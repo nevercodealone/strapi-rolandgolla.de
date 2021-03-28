8566b7b4dd9d394ad7f2ba228b22e96341bd429f
Roland Golla <rolandgolla@gmail.com>
2021-03-28 01:06:41 +0100

    actual state

--- gridsome/gridsome.config.js
+++ gridsome/gridsome.config.js
@@ -1,30 +1,38 @@
-const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
-const axios = require('axios')
+const tailwind = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const axios = require("axios");
 
-const postcssPlugins = [
-  tailwind(),
-]
+const postcssPlugins = [tailwind()];
 
-if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
+if (process.env.NODE_ENV === "production")
+  postcssPlugins.push(purgecss(require("./purgecss.config.js")));
 
 module.exports = {
-  siteName: 'Roland Golla',
+  siteName: "Roland Golla",
   plugins: [
     {
       use: "gridsome-plugin-tailwindcss",
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        path: 'docs/**/*.md',
-        typeName: 'StartPage'
-      }
-    }
+        path: "docs/**/*.md",
+        typeName: "StartPage",
+      },
+    },
+    {
+      use: "@noxify/gridsome-plugin-remote-image",
+      options: {
+        typeName: "Entry",
+        sourceField: "remoteImages",
+        targetField: "imagesDownloaded",
+        targetPath: "./src/assets/remoteImages",
+      },
+    },
   ],
   css: {
-      loaderOptions: {
-          postcss: {
-              plugins: postcssPlugins,
-          },
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins,
       },
+    },
   },
-}
+};
