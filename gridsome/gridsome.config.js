7a2d394fb1884d9f4e4b649d7136ddb5094cc084
Roland Golla <rolandgolla@gmail.com>
2021-03-28 23:29:35 +0200

    actual state

--- gridsome/gridsome.config.js
+++ gridsome/gridsome.config.js
@@ -11,6 +11,18 @@ module.exports = {
   siteName: "Roland Golla",
   plugins: [
     {
+      use: "@gridsome/source-strapi",
+      options: {
+        apiURL: "http://localhost:1337",
+        queryLimit: 1000, // Defaults to 100
+        singleTypes: ["StartPage"],
+        loginData: {
+          identifier: "",
+          password: "",
+        },
+      },
+    },
+    {
       use: "gridsome-plugin-tailwindcss",
       use: "@gridsome/source-filesystem",
       options: {
@@ -21,8 +33,8 @@ module.exports = {
     {
       use: "@noxify/gridsome-plugin-remote-image",
       options: {
-        typeName: "Entry",
-        sourceField: "remoteImages",
+        typeName: "UploadFile",
+        sourceField: "url",
         targetField: "imagesDownloaded",
         targetPath: "./src/assets/remoteImages",
       },
